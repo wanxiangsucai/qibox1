c4bc34f9e4a3fcd5a5102fbc14d16eac82d79aec
齐博 <2244484@qq.com>
2018-05-21 15:53:45 +0800

    数据库升级

--- application/common/upgrade/10.sql
+++ application/common/upgrade/10.sql
@@ -1,1 +1,3 @@
 ALTER TABLE `qb_msg` DROP INDEX  `touid` ,ADD INDEX  `touid` (  `touid` ,  `ifread` );
+
+ALTER TABLE  `qb_hook_plugin` ADD  `version` VARCHAR( 60 ) NOT NULL COMMENT  '版本信息',ADD  `version_id` MEDIUMINT( 7 ) NOT NULL COMMENT  '云端对应的ID';
--- application/install/sql/install.sql
+++ application/install/sql/install.sql
@@ -1567,3 +1567,5 @@ CREATE TABLE IF NOT EXISTS `qb_weixinyznum` (
 ) ENGINE=MEMORY DEFAULT CHARSET=utf8;
 
 ALTER TABLE `qb_msg` DROP INDEX  `touid` ,ADD INDEX  `touid` (  `touid` ,  `ifread` );
+
+ALTER TABLE  `qb_hook_plugin` ADD  `version` VARCHAR( 60 ) NOT NULL COMMENT  '版本信息',ADD  `version_id` MEDIUMINT( 7 ) NOT NULL COMMENT  '云端对应的ID';
