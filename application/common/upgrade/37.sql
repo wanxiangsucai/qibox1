3b48d36c123c99fc3d2f5d4d585e758fe68896be
齐博 <2244484@qq.com>
2018-09-21 17:02:15 +0800

    no commit message

---
+++ application/common/upgrade/37.sql
@@ -0,0 +1,6 @@
+UPDATE  `qb_module` SET  `version_id` =  '2' WHERE `keywords` = 'form';
+UPDATE  `qb_module` SET  `version_id` =  '3' WHERE `keywords` = 'links';
+UPDATE  `qb_module` SET  `version_id` =  '5' WHERE `keywords` = 'bbs';
+UPDATE  `qb_module` SET  `version_id` =  '6' WHERE `keywords` = 'hongbao';
+UPDATE  `qb_module` SET  `version_id` =  '9' WHERE `keywords` = 'qiniu';
+UPDATE  `qb_module` SET  `version_id` =  '11' WHERE `keywords` = 'search';
