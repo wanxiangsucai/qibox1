257e19ac8225e1424200188e48d9a7c7fce13984
齐博 <2244484@qq.com>
2018-10-24 14:48:11 +0800

    no commit message

--- application/install/sql/install.sql
+++ application/install/sql/install.sql
@@ -1699,3 +1699,16 @@ ALTER TABLE  `qb_weixinword` COMMENT =  '公众号回复关键字响应的内容
 INSERT INTO `qb_config` (`id`, `type`, `title`, `c_key`, `c_value`, `form_type`, `options`, `ifsys`, `htmlcode`, `c_descrip`, `list`, `sys_id`) VALUES(0, 1, 'WAP端是否指定某个频道做为默认主页', 'set_module_wapindex', '0', 'select', 'app\\common\\util\\Module@getTitleList@["不特别指定","keywords"]', 1, '', '', 0, 0);
 
 ALTER TABLE `qb_rmb_infull` ADD INDEX (  `uid` ,  `ifpay` );
+
+
+ALTER TABLE  `qb_shop_field` ADD  `script` TEXT NOT NULL COMMENT  'JS脚本',ADD  `trigger` VARCHAR( 255 ) NOT NULL COMMENT  '选择某一项后,联动触发显示其它字段';
+
+UPDATE `qb_shop_field` SET `about` =  '只有这一项可以定义价格,属性|价格用竖线隔开,比如:大份|20' WHERE `name` = 'type1';
+UPDATE `qb_shop_field` SET `about` =  '<script>if($("#atc_myfid").children().length<1)$("#form_group_myfid").remove();</script>' WHERE `name` = 'myfid';
+
+INSERT INTO `qb_shop_field` (`id`, `name`, `title`, `type`, `field_type`, `value`, `options`, `about`, `show`, `mid`, `ajax_url`, `next_items`, `param`, `format`, `table`, `level`, `key`, `option`, `pid`, `list`, `listshow`, `ifsearch`, `ifmust`, `nav`, `input_width`, `input_height`, `unit`, `match`, `css`, `script`, `trigger`) VALUES(0, 'market_price', '市场原价', 'money', 'decimal(10,2) unsigned NOT NULL', '', '', '', 1, 1, '', '', '', '', '', 2, '', '', '', 99, 0, 0, 0, '', '', '', '', '', '', '', '');
+INSERT INTO `qb_shop_field` (`id`, `name`, `title`, `type`, `field_type`, `value`, `options`, `about`, `show`, `mid`, `ajax_url`, `next_items`, `param`, `format`, `table`, `level`, `key`, `option`, `pid`, `list`, `listshow`, `ifsearch`, `ifmust`, `nav`, `input_width`, `input_height`, `unit`, `match`, `css`, `script`, `trigger`) VALUES(0, 'num', '库存量', 'number', 'mediumint(7) NOT NULL DEFAULT ''0''', '500', '', '', 1, 1, '', '', '', '', '', 2, '', '', '', 97, 0, 0, 0, '', '', '', '', '', '', '', '');
+
+ALTER TABLE `qb_shop_content1` ADD  `num` MEDIUMINT( 6 ) NOT NULL DEFAULT  '500' COMMENT  '库存量';
+ALTER TABLE `qb_shop_content1` ADD  `market_price` DECIMAL( 10, 2 ) NOT NULL COMMENT  '市场原价';
+UPDATE `qb_shop_content1` SET  `num` =20;
