eb7409d284b31d12dbeaecd4d277f0f906876727
齐博 <2244484@qq.com>
2018-12-26 14:52:29 +0800

    no commit message

--- application/common/model/Info.php
+++ application/common/model/Info.php
@@ -65,91 +65,15 @@ abstract class Info extends Model
         }
     }
     
-    //获取所有栏目的名称及ID
-    public static function getTitleList($where=[])
-    {
-        static $list = NULL;
-        if($list==NULL){
-            $list = self::where($where)->column('id,name');
-        }
-        return $list;
+    /**
+     * 根据栏目FID获取本栏目下的所有内容
+     * @param number $fid
+     * @return array
+     */
+    public static function getAllByfid($fid=0){
+        self::InitKey();
+        $listdb = Db::name(self::$model_key.'_info')->where('cid','=',$fid)->order('list DESC,id DESC')->column('id,aid');
+        return $listdb;
     }
-    
-    //通过ID得到相应的标题名称
-    public static function getNameById($id)
-    {
-        if (empty($id)) {
-            return ;
-        }
-        $list = static::getTitleList();
-        if($list){
-            return $list[$id];
-        }
-    }
-    
-    //获取一个值值给某些地方没有指定MID的地方默认使用
-    public static function getId()
-    {
-        $list = static::getTitleList();
-        if($list){
-            return current(array_flip($list));
-        }
-    }
-    
-    //第一项，指定ID及其子ID不要显示，比如创建栏目的时候容易造成死循环，第二项发布文章的时候，不能选择其它模型的栏目
-    public static function getTreeTitle($id = 0, $mid = 0,$default_title = '请选择...')
-    {
-        $where = [];
-        $result = [];
-        if ($default_title != '') {
-            $result[0] = $default_title;
-        }
-        
-        if ($id !== 0) {
-            $hide_ids    = array_merge([$id], static::getSonsId($id));
-            $where['id'] = ['notin', $hide_ids];
-        }
-        
-        if ($mid !== 0) {
-            $where['mid'] = $mid;
-        }
-        
-        $data_list = Tree::config(['title' => 'name'])->toList(self::where($where)->order('list desc,id desc')->column('id,cid,aid'));
-        foreach ($data_list as $item) {
-            $result[$item['id']] = $item['title_display'];
-        }
-        
-        if ($default_title === false) {
-            unset($result[0]);
-        }
-        
-        return $result;
-    }
-    
-    public static function getTreeList($id = 0, $mid = 0)
-    {
-        $where = [];
-        $result = [];
-        
-        if ($id !== 0) {
-            $hide_ids    = array_merge([$id], static::getSonsId($id));
-            $where['id'] = ['notin', $hide_ids];
-        }
-        if ($mid !== 0) {
-            $where['mid'] = $mid;
-        }
-        
-        $data_list = Tree::config(['title' => 'name'])->toList(self::where($where)->order('list desc,id desc')->column('id,cid,aid'));
-     
-        return $data_list;
-    }
-    
-    public static function getSonsId($id = 0)
-    {
-        $array = $id_array = self::where('pid', $id)->column('id');
-        foreach ($id_array AS $id_value) {
-            $array = array_merge($array, static::getSonsId($id_value));
-        }
-        return $array;
-    }
+
 }
