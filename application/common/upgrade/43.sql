c98449a6a8bbebc3b9c3d4ded1152605024b0550
齐博 <2244484@qq.com>
2019-01-09 09:44:02 +0800

    no commit message

--- application/common/upgrade/43.sql
+++ application/common/upgrade/43.sql
@@ -1,1 +1,1 @@
-UPDATE `qb_config` SET  `form_type`='usergroup',`c_value`=''  WHERE `c_key`='getout_percent_money';
+UPDATE `qb_config` SET  `form_type`='usergroup',`c_value`=''  WHERE `c_key`='getout_percent_money'; 
