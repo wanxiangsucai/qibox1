df7b65f316dc398e6346fab8a575f8e057e1429b
齐博 <2244484@qq.com>
2019-02-18 19:54:20 +0800

    no commit message

--- application/admin/controller/Upgrade.php
+++ application/admin/controller/Upgrade.php
@@ -64,6 +64,7 @@ class Upgrade extends AdminBase
 	    $this->upgrade_mark($data['index_style'],'index_style');
 	    $this->upgrade_mark($data['member_style'],'member_style');
 	    $this->upgrade_mark($data['qun_style'],'qun_style');
+	    $this->upgrade_mark($data['haibao_style'],'haibao_style');
 	    
 	    if( file_put_contents(config('client_upgrade_edition'), '<?php return ["md5"=>"'.$upgrade_edition.'","time"=>"'.date('Y-m-d H:i').'",];') ){
 	        return true;
