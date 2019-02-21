bdd9d87806c11e930927e764385a308086dfae02
齐博 <2244484@qq.com>
2019-02-21 20:06:51 +0800

    no commit message

--- application/common/upgrade/U11.php
+++ application/common/upgrade/U11.php
@@ -4,17 +4,17 @@ namespace app\common\upgrade;
 class U11{
 	public function up(){
 	    set_time_limit(0);
-	    @mkdir(PUBLIC_PATH.'static/images/haibao_style');
-	    @mkdir(PUBLIC_PATH.'static/images/haibao_style/default');
+	    @mkdir(PUBLIC_PATH.'static/haibao_style');
+	    @mkdir(PUBLIC_PATH.'static/haibao_style/default');
 	    
-		copy('https://x1.php168.com/public/static/images/haibao_style/default/demo.jpg',
-		         PUBLIC_PATH.'static/images/haibao_style/default/demo.jpg');
+		copy('https://x1.php168.com/public/static/haibao_style/default/demo.jpg',
+		         PUBLIC_PATH.'static/haibao_style/default/demo.jpg');
 		
-		copy('https://x1.php168.com/public/static/images/haibao_style/default/haibao_hua.png',
-		        PUBLIC_PATH.'static/images/haibao_style/default/haibao_hua.png');
+		copy('https://x1.php168.com/public/static/haibao_style/default/haibao_hua.png',
+		        PUBLIC_PATH.'static/haibao_style/default/haibao_hua.png');
 		
-		copy('https://x1.php168.com/public/static/images/haibao_style/default/haibao_bg.png',
-		        PUBLIC_PATH.'static/images/haibao_style/default/haibao_bg.png');
+		copy('https://x1.php168.com/public/static/haibao_style/default/haibao_bg.png',
+		        PUBLIC_PATH.'static/haibao_style/default/haibao_bg.png');
 		 
 	}
 }
