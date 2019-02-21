e76802d2dcf1f5e5c885061ff7cbcbf656c2d932
齐博 <2244484@qq.com>
2019-02-21 09:24:53 +0800

    no commit message

--- application/common/upgrade/U10.php
+++ application/common/upgrade/U10.php
@@ -4,9 +4,11 @@ use think\Db;
 
 class U10{
 	public function up(){
+		/*
 	    $info = Db::name('config')->where('c_key','money_ratio')->find();
 	    if (empty($info)) {
 	        into_sql("INSERT INTO `qb_config` (`id`, `type`, `title`, `c_key`, `c_value`, `form_type`, `options`, `ifsys`, `htmlcode`, `c_descrip`, `list`, `sys_id`) VALUES(0, 32, '1块钱兑换多少个积分', 'money_ratio', '10', 'number', '', 0, '', '', 0, -5);");
 	    }
+		*/
 	}
 }
