0d885dd2dbe82121254b38b6001ccb1e9fcacdb8
齐博 <1663089+qibosoft@users.noreply.gitee.com>
2019-05-14 17:26:19 +0800

    改进百度地图自动定位的功能

--- application/common/field/bmap/form.php
+++ application/common/field/bmap/form.php
@@ -1,8 +1,13 @@
 <?php
 function_exists('urls') || die('ERR');
-$info[$name] || $info[$name] = '113.268332,23.130274';
+$info[$name] || $info[$name] = '113.263661,23.155131';
 $jscode = '';
 if(fun('field@load_js',$field['type'])){
+	$weixin_appid = weixin_share("appId");
+	$weixin_time = weixin_share("timestamp")?:0;
+	$weixin_nonceStr = weixin_share("nonceStr");
+	$weixin_signature = weixin_share("signature");
+	$qun_url = iurl('qun/near/point_address');
 	$jscode = <<<EOT
 <style type="text/css">
 .bmap{width:100%;height:350px;border: 1px solid #ccc;}
@@ -12,6 +17,9 @@ if(fun('field@load_js',$field['type'])){
 <script type="text/javascript" src="https://api.map.baidu.com/api?v=2.0&ak=MGdbmO6pP5Eg1hiPhpYB0IVd"></script>
 <script type="text/javascript">
 jQuery(document).ready(function() {
+	set_baidu_map();
+});
+function set_baidu_map(){
     $('.js-bmap').each(function() {
         var that = $(this);
         var map_canvas = that.find('.bmap').attr('id');
@@ -120,9 +128,102 @@ jQuery(document).ready(function() {
         if (address.val() != '') {
             ac.setInputValue(address.val())
         }
+
+		if(map_point.val() == '113.263661,23.155131'||map_point.val() == ''){	//新发表的时候，获取当前定位坐标
+			get_location_point(map_point);
+		}
     });
-});
+}
  </script>
+
+<script type="text/javascript" src="https://developer.baidu.com/map/jsdemo/demo/convertor.js"></script>
+<script type="text/javascript" src="https://res.wx.qq.com/open/js/jweixin-1.3.2.js"></script>
+<script type="text/javascript" src="__STATIC__/js/map-gps.js"></script>
+<script type="text/javascript">
+//公众号或WAP浏览器获取当前坐标位置
+function reload_baidu_map(obj){
+	var geolocation = new BMap.Geolocation();
+	geolocation.getCurrentPosition(function(result){
+		if(this.getStatus() == window.BMAP_STATUS_SUCCESS){
+		  map_x = result.point.lng;
+		  map_y = result.point.lat;
+		  obj.val(map_x+','+map_y);
+		  if(typeof(set_baidu_map)=='function'){set_baidu_map();}
+
+		  //显示当前位置的街道名
+			var geoc = new BMap.Geocoder();
+			geoc.getLocation(result.point, function(rs){
+				var addComp = rs.addressComponents;
+				$("#atc_address").val(addComp.district + addComp.street + addComp.streetNumber);
+			});
+
+			//gg = GPS.bd_decrypt(result.point.lat, result.point.lng);	//百度转谷歌
+			//wgs = GPS.gcj_decrypt(gg.lat, gg.lon); //谷歌转GPS
+		    //showMapPosition(wgs.lon,wgs.lat);
+		} else {
+			alert('failed:'+this.getStatus());
+		}        
+	},{enableHighAccuracy: true})
+}
+
+//小程序获取当前坐标位置
+function load_wx_map(obj){ 
+	wx.getLocation({
+			type: 'gcj02', // 默认为wgs84的gps坐标，如果要返回直接给openLocation用的火星坐标，可传入'' 
+      success: function (res) {
+		  var latitude = res.latitude; // 纬度，浮点数，范围为90 ~ -90
+          var longitude = res.longitude; // 经度，浮点数，范围为180 ~ -180。
+			var o = GPS.bd_encrypt(res.latitude,res.longitude);
+           map_x = o.lon;//res.longitude;
+		   map_y = o.lat;//res.latitude; 
+		  obj.val(map_x+','+map_y);
+		  if(typeof(set_baidu_map)=='function'){set_baidu_map();}		  
+		  $.get("{$qun_url}?x=" + map_y +  "&y=" + map_x  ,function(res){
+			  $("#atc_address").val(res.data);
+		  });
+      },
+      cancel: function (res) {
+        alert('用户拒绝授权获取地理位置');
+      }
+    }); 
+}
+
+var is_in_wxapp = -1;
+function get_location_point(obj){
+	wx.miniProgram.getEnv(function(res) {
+		if(res.miniprogram==true){	//在小程序中,用不了百度地图定位
+			is_in_wxapp = 1;
+			wx.config({
+				debug: false,
+				appId: '{$weixin_appid}',
+				timestamp: {$weixin_time},
+				nonceStr: '{$weixin_nonceStr}',
+				signature: '{$weixin_signature}',
+				jsApiList: [
+						'openLocation',
+						'getLocation'
+				]
+			});
+			wx.ready(function () {
+				load_wx_map(obj);
+			});
+			wx.error(function (res) {
+				alert(res.errMsg);
+			});		
+		}else{
+			is_in_wxapp = 0;
+			reload_baidu_map(obj); //在公众号或WAP浏览器里
+		}
+	});
+	setTimeout(function(){
+			if(is_in_wxapp==-1){
+				reload_baidu_map(obj);
+			}
+		},1500);
+}
+
+</script>
+
 EOT;
 }
 return <<<EOT
@@ -131,7 +232,7 @@ $jscode
 		<input class="bmap-address" style="width:85%;float: left;" id="bmap-address-{$name}" name="{$name}_address" type="text" value="" placeholder="请输入要搜索的地址,或者手工在下面精准定位"> 
 		<div id="baaapsou-{$name}" class="baaapsou">搜索</div>
         <div class="searchResultPanel"></div>
-        <input class="bmap-point" type="hidden" id="atc_{$name}" name="{$name}" value="{$info[$name]}">
+        <input class="bmap-point" type="text" id="atc_{$name}" name="{$name}" value="{$info[$name]}">
         <div class="bmap" id="bmap-canvas-{$name}"></div>			
 </div>
 EOT;
