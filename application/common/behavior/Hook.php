55392616deeba54091d17441773284619b590d65
齐博 <1663089+qibosoft@users.noreply.gitee.com>
2019-05-20 20:58:41 +0800

    no commit message

--- application/common/behavior/Hook.php
+++ application/common/behavior/Hook.php
@@ -24,18 +24,23 @@ class Hook
         $hook_plugins = cache('hook_plugins');
         $hooks = cache('hooks');
 
-        if (!$hook_plugins&&is_table('hook')) {
+        if (!$hook_plugins) {
             // 所有接口
             $hooks = HookModel::where('ifopen', 1)->column('id,name');
             //所有接口功能
-            $hook_plugins = HookPluginModel::where('ifopen', 1)->column(true);
+            $hook_plugins = HookPluginModel::where('ifopen', 1)->order('list desc,id asc')->column(true);
             cache('hook_plugins', $hook_plugins);
             cache('hooks', $hooks);
         }
-        
+        $array = [];
         //接口关闭的话,该接口下的所有钩子都禁用
         foreach ($hook_plugins as $rs) {
             if ( in_array($rs['hook_key'], $hooks) ) {
+                $key = $rs['hook_key'].'-'.$rs['hook_class'];
+                if ($array[$key]===true) {
+                    continue ;  //避免重复安装的钩子，重复执行
+                }
+                $array[$key] = true;
                 if($rs['plugin_key'] && empty(plugins_config($rs['plugin_key']))){
                     //continue ; //如果插件卸载掉的话,就不要执行了
                 }
