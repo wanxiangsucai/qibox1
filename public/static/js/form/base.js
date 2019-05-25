61d9f75cc0639538147ffdf43f7ce51b7b60a2c9
齐博 <1663089+qibosoft@users.noreply.gitee.com>
2019-05-25 20:47:09 +0800

    no commit message

--- public/static/js/form/base.js
+++ public/static/js/form/base.js
@@ -41,72 +41,41 @@ jQuery(document).ready(function() {
     if (typeof(trigger_config)=='object' && typeof(trigger_config.triggers) != 'undefined') {
         // 先隐藏依赖项
         var field_hide_array   = trigger_config.field_hide.split(',') || [];
-        var field_values_array = trigger_config.field_values.split(',') || [];
-        for (var index in field_hide_array) {
+        for (var index in field_hide_array){
             $('#form_group_'+field_hide_array[index]).hide();
         }
-
         var trigger_form = $('.form-trigger');
-
         $.each(trigger_config.triggers, function (trigger, content) {
             trigger_form.delegate('[name='+ trigger +']', 'change', function (event, init) {
                 var trigger_item = $(this);
                 var trigger_value   = trigger_item.val();
+				
+				for (var index in field_hide_array) {
+					$('#form_group_'+field_hide_array[index]).hide();
+				}
 
                 $(content).each(function () {
                     var that = $(this);
-                    var sel_values  = that[0].split(',') || [];
-                    var targets_array = that[1].split(',') || [];
-
-                    // 如果触发的元素是单选，且没有选中则设置值为空
-                    if (trigger_item.attr('type') == 'radio' && trigger_item.is(':checked') == false) {
-                        trigger_value = '';
-                    }
-
-                    if ($.inArray(trigger_value, sel_values) >= 0) {
-                        // 符合指定的值，显示对应的表单项
+                    var sel_values  = that[0];
+                    var targets_array = that[1].split(',');
+                    if (trigger_value==sel_values) {
                         for (var index in targets_array) {
-                            // 如果不是该对象自身直接创建的属性（也就是该属//性是原型中的属性），则跳过显示
-                            if (!targets_array.hasOwnProperty(index)) {
-                                continue;
-                            }
-							var ck = true;
-							var fname = targets_array[index];
-							for(var kname in trigger_config[fname]){								
-								var varray = (trigger_config[fname][kname]).split(',');
-								var val = $("#atc_"+kname).val();
-								if(val==undefined){
-									val = $("input[name="+kname+"]:checked").val();
-								}								
-								if( $.inArray( val , varray) ==-1 ){
-									ck = false;
-								}								
-								//console.log('wrf='+kname, $.inArray('text', varray) );
-							}							
-                            if(ck==true)$('#form_group_'+targets_array[index]).show();
-                        }
-                    } else {
-                        for (var item in targets_array) {
-                            if (!targets_array.hasOwnProperty(item)) {
-                                continue;
-                            }
-							$('#form_group_'+targets_array[item]).hide();
+							$('#form_group_'+targets_array[index]).show();
                         }
                     }
                 });
             });
-
-            // 有默认值时触发
+            // 有默认值时
             var trigger_value = '';
-            if (trigger_form.find('[name='+ trigger +']').attr('type') == 'radio') {
+			if (trigger_form.find('[name='+ trigger +']').attr('type') == 'radio') {
                 trigger_value = trigger_form.find('[name='+ trigger +']:checked').val() || '';
-                if (trigger_value != '' && $.inArray(trigger_value, field_values_array) >= 0) {
+                if (trigger_value != '') {
                     var $radio_id = $('.form-trigger [name='+ trigger +']:checked').attr('id');
-                    $('.form-trigger #'+$radio_id).trigger("change", ['1']);
+                    $('.form-trigger #'+$radio_id).trigger("change");
                 }
             } else {
                 trigger_value = trigger_form.find('[name='+ trigger +']').val() || '';
-                if (trigger_value != '' && $.inArray(trigger_value, field_values_array) >= 0) {
+                if (trigger_value != '') {
                     $('.form-trigger [name='+ trigger +']').trigger("change");
                 }
             }
