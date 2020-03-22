2084e7e27b3a88a514198c690986f71567747264
齐博 <2244484@qq.com>
2020-03-22 09:49:57 +0800

    no commit message

--- application/common/fun/Bbs.php
+++ application/common/fun/Bbs.php
@@ -4,13 +4,29 @@ namespace app\common\fun;
 class Bbs{
     
     /**
+     * 兼容旧模板,使用缓存,提高效率
+     * @param number $id
+     * @param string $content
+     * @return string
+     */
+    public static function content_cache($id=0,$content=''){
+        static $array = [];
+        if ($content!='') {
+            $array[$id] = $content;
+        }else{
+            return $array[$id];
+        }        
+    }
+    
+    /**
      * 获取贴子内容
      * @param number $id
      * @param number $leng
      * @return unknown|string
      */
     public static function getContents($id=0,$leng=0){
-        $contents = query('bbs_contents')->where('id',$id)->value('content');
+        $contents = self::content_cache($id) ?: query('bbs_contents')->where('id',$id)->value('content');
+        self::content_cache($id,$contents);
         if ($leng>0) {
             $contents = get_word(del_html($contents),$leng);
         }
