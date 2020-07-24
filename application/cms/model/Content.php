78682c429e710c90b900400b9a40fdb26d1d14a8
齐博 <2244484@qq.com>
2020-07-24 08:12:19 +0800

    no commit message

--- application/cms/model/Content.php
+++ application/cms/model/Content.php
@@ -15,8 +15,9 @@ class Content extends C
     public static function deleteData($id=0,$mid=0){
         $result = parent::deleteData($id,$mid);
         if ($result===true && class_exists('Pages')) { //必须绝对等于,因为1的时候是软删除
-            Pages::where('aid',$id)->delete();
+            Pages::where('aid',$id)->delete();            
         }
+        Info::where('aid',$id)->delete();
         return $result;
     }
 }
