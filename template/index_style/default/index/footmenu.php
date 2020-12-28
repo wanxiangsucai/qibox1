3bebc41ed60b35ea38c442fe1e27a6969d025dde
齐博 <2244484@qq.com>
2020-12-28 15:51:29 +0800

    no commit message

--- template/index_style/default/index/footmenu.php
+++ template/index_style/default/index/footmenu.php
@@ -3,19 +3,22 @@
 return [
     'title'=>'底部菜单相关功能设置',
 	'form'=>[
+		['radio','hidelay','点击小圆点显示大窗','',['启用','不启用']],
+		['color','bgcolor','小圆点背景色','','#1bb0d2'],
 		['text','title1','第一行标题','','随时随地交流，掌控身边动态！'],
 		['text','title2','第二行标题','','创建属于自己的圈子'],
 		['text','title3','第三行标题','','发现生活乐圈，绽放精彩人生'],
 		['text','title4','第四行标题','','点击下载APP，可享受更多权益'],
-		['text','link1','链接一','',murl('qun/content/add',['mid'=>1])],
+		['text','link1','链接一','',murl('qun/content/add',['mid'=>1])],		
+		['radio','hidedownapp','是否提示APP下载','',['显示','隐藏']],
 		['file','appurl','app下载地址',''],
-		['radio','hidelay','是否显示层','',['显示','隐藏']],
+		['radio','hide2icon','是否显示二级菜单图标','',['显示','隐藏']],
 	],
 	//'help_msg'=>'帮助信息',
 	//联动显示
 	'trigger'=>[
-		//['type1','0','test1'],	//第二第三项,多个用逗号隔开
-		//['type3','0','test2,test4'],
+		['hidelay','0','bgcolor,title1,title2,title3,title4,link1,appurl,hidedownapp'],	//第二第三项,多个用逗号隔开
+		['hidelay','1','hide2icon'],
 	],
 	'type1'=>'', //wap 或 pc
 	'type2'=>'', //www 或 hy 
