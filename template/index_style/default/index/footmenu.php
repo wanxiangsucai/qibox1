9bc8dd840f02385aaff207c28fea4dfd49e20873
齐博 <2244484@qq.com>
2020-12-28 16:12:03 +0800

    no commit message

--- template/index_style/default/index/footmenu.php
+++ template/index_style/default/index/footmenu.php
@@ -4,7 +4,7 @@ return [
     'title'=>'底部菜单相关功能设置',
 	'form'=>[
 		['radio','hidelay','点击小圆点显示大窗','',['启用','不启用']],
-		['color','bgcolor','小圆点背景色','','#1bb0d2'],
+		['color','pointbgcolor','小圆点背景色','','#1bb0d2'],
 		['text','title1','第一行标题','','随时随地交流，掌控身边动态！'],
 		['text','title2','第二行标题','','创建属于自己的圈子'],
 		['text','title3','第三行标题','','发现生活乐圈，绽放精彩人生'],
@@ -17,7 +17,7 @@ return [
 	//'help_msg'=>'帮助信息',
 	//联动显示
 	'trigger'=>[
-		['hidelay','0','bgcolor,title1,title2,title3,title4,link1,appurl,hidedownapp'],	//第二第三项,多个用逗号隔开
+		['hidelay','0','pointbgcolor,title1,title2,title3,title4,link1,appurl,hidedownapp'],	//第二第三项,多个用逗号隔开
 		['hidelay','1','hide2icon'],
 	],
 	'type1'=>'', //wap 或 pc
