718647070a7c7853603ff9eea5cb3c4891d8cd89
齐博 <2244484@qq.com>
2021-12-28 10:36:13 +0800

    no commit message

--- application/cms/admin_menu.php
+++ application/cms/admin_menu.php
@@ -1,7 +1,7 @@
 <?php
 
-if(config('webdb.typeid')&&is_file(__DIR__.'/'.config('webdb.typeid').'member_menu.php')){
-    return include_once __DIR__.'/'.config('webdb.typeid').'member_menu.php';
+if(config('webdb.typeid')&&is_file(__DIR__.'/'.config('webdb.typeid').'admin_menu.php')){
+    return include_once __DIR__.'/'.config('webdb.typeid').'admin_menu.php';
 }
 
 return array(
@@ -19,25 +19,10 @@ return array(
 								        array(
 								                'title'=>'发布内容',
 								                'link'=>'content/postnew',
-    								            'power'=>[
-    								                'pages/index'=>'分页管理',
-    								                'pages/add'=>'添加分页',
-    								                'pages/edit'=>'修改分页',
-    								                'pages/delete'=>'删除分页',
-    								            ],
 								            ),
 								        array(
 								                'title'=>'内容管理',
 								                'link'=>'content/index',
-    								            'power'=>[
-    								                'add'=>'发布内容',
-    								                'edit'=>'修改内容',
-    								                'delete'=>'删除内容',
-    								                'index_fid'=>'更改栏目',
-    								                'index_view'=>'更改浏览量',
-    								                'index_list'=>'更改排序值',
-    								                'index_status'=>'更改状态',
-    								            ],
 								              ),
     									array(
     										'title'=>'栏目管理',
@@ -69,26 +54,11 @@ return array(
 								                'link'=>['sort_field/index',['mid'=>-2]],
 								                //'power'=>['edit','delete'],
 								        ),
-								        array(
-								                'title'=>'辅栏目字段管理',
-								                'link'=>['sort_field/index',['mid'=>-3]],
-								                //'power'=>['edit','delete'],
-								        ) ,
-    								    array(
-    								        'title'=>'创作收益日志',
-    								        'link'=>'sell_log/index',
-											'power'=>['delete'],
-    								    ),
-								        array(
-								                'title'=>'用户分享记录',
-								                'link'=>['fxuser/index'],
-								                'power'=>['delete'],
-								        ),
-								        array(
-								                'title'=>'用户分享收益',
-								                'link'=>['fxlog/index'],
-								                'power'=>['delete'],
-								        ),
+// 								        array(
+// 								                'title'=>'辅栏目字段管理',
+// 								                'link'=>['sort_field/index',['mid'=>-3]],
+// 								                //'power'=>['edit','delete'],
+// 								        ),
 								),
 							),
 				),
--- application/cms/member_menu.php
+++ application/cms/member_menu.php
@@ -5,6 +5,8 @@ if(config('webdb.typeid')&&is_file(__DIR__.'/'.config('webdb.typeid').'member_me
     return include_once __DIR__.'/'.config('webdb.typeid').'member_menu.php';
 }
 
+//菜单权限教程 https://www.kancloud.cn/php168/x1_of_qibo/816623
+
 return array(
 		'cms'=>array(
 				'title'=>'cms',
@@ -22,46 +24,16 @@ return array(
 								        'link'=>'content/postnew',
 								        'power'=>'can_post_group',
 								    ),
-								    array(
+								    /*array(
 								        'title'=>'采集公众号文章',
 								        'link'=>'content/copynews',
 								        'power'=>'can_post_group',
-								    ),
+								    ),*/
 								    array(
 								        'title'=>'分类管理',
 								        'link'=>'mysort/index',
 								        'power'=>'can_post_group',
-								    ),    									
-								    array(
-								        'title'=>'创作收益',
-								        'link'=>'sell_log/index',
-								        'power'=>'can_post_group',
 								    ),
-								    array(
-								        'title'=>'我要分享',
-								        'link'=>'content/fx',
-								        'power'=>'allow_fx_group',
-								    ),
-								    array(
-								        'title'=>'我创建的优惠券',
-								        'link'=>'youhui/index',
-								        'power'=>'allow_fx_group',
-								    ),
-								    array(
-								        'title'=>'我领取的优惠券',
-								        'link'=>'yhlog/index',
-								         
-								    ),
-								    array(
-								            'title'=>'我的分享记录',
-								            'link'=>'fxuser/index',
-								            'power'=>'allow_fx_group',
-								    ),
-								    array(
-								                'title'=>'我的分享收益',
-								                'link'=>'fxlog/index',
-								                'power'=>'allow_fx_group',
-								     ),
 								    array(
 								        'title'=>'主题审核管理',
 								        'link'=>'content/manage',
