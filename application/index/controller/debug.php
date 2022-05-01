df953c24743fa34b719e264837667d4953a26330
齐博 <2244484@qq.com>
2022-05-01 17:13:11 +0800

    方便用户截图重要的报错信息

--- application/index/controller/debug.php
+++ application/index/controller/debug.php
@@ -324,6 +324,7 @@
                     <h2>[<?php echo $code; ?>]&nbsp;<?php echo sprintf('%s in %s', parse_class($name), parse_file($file, $line)); ?></h2>
                 </div>
                 <div><h1><?php echo nl2br(htmlentities($message)); ?></h1></div>
+                <div><?php if(!empty($datas)&&$datas['Database Status'])echo $datas['Database Status']['Error SQL'];?></div>
             </div>
         
     </div>
